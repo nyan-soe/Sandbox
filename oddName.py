a85aeb47c5be90e432d1d39d1b26ae140ebc4022
nyan-soe <58061109+nyan-soe@users.noreply.github.com>
2019-11-28 23:14:49 +0800

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,22 @@
 """Nyan Soe"""
+
+
+def main():
+    name = input("Your name:")
+    name = name.strip()
+    while name == "":
+        print("Name can not be blank")
+        name = input("Your name:")
+        name = name.strip()
+
+    words = name.split()
+
+    for word in words:
+        count = 0
+        for character in word:
+            if count == 1:
+                print(character)
+            count += 1
+
+
+main()
